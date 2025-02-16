8b556f235b395d254ed8a35dddfb0a76a03790ae
anatsrii <anat.allsolution@gmail.comgit config user.name anatsriigit config user.email git config --global user.email anat.allsolution@gmail.com>
2025-02-16 18:45:23 +0700

    User router has completed

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,15 +1,18 @@
 const express = require('express');
 require('bcrypt');
 const { login } = require('../controllers/users/login');
+const { getAllUsers } = require('../controllers/users/getAllUsers.js');
 const { getUser } = require('../controllers/users/getUser.js');
 const { updateUser, updatePassword } = require('../controllers/users/updateUser');
 const { deleteUser } = require('../controllers/users/deleteUser');
 const { registerUser } = require('../controllers/users/registerUser.js');
 
 
+
 const router = express.Router();
 
 // user routes
+router.get('/all', getAllUsers);
 router.post('/register', registerUser);
 router.get('/:id', getUser);
 router.post('/login', login);
